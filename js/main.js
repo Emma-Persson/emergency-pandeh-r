330d82a5422d38ba8bb8df88a163e5229cf5b06a
Emma Persson <empe0009@stud.ek.dk>
2025-11-04 14:49:04 +0100

    new

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// document.querySelector("#hotspot1").addEventListener("click", updateInfoboxHead);
+// function updateInfoboxHead() {
+//   console.log("updateInfoboxHead");
+//   document.querySelector("#efficiency").innerHTML = "<h2>test</h2>";
+//   document.querySelector(".info-text .placeholder").textContent = "her er info";
+// }
+// <img src='billede.webp'>""
+document.querySelector("#hotspot1").addEventListener("click", updateInfoboxHead);
+function updateInfoboxHead() {
+  console.log("updatInfoboxHead");
+  document.querySelector("#efficiency").textContent = "Her er info";
+}

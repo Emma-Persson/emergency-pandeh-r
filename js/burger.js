2b76c651ca9915f502078f67a4794a3267a36d0f
Emma Persson <empe0009@stud.ek.dk>
2025-11-09 16:15:05 +0100

    forms

--- js/burger.js
+++ js/burger.js
@@ -6,3 +6,28 @@ function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
+
+function cancelPopup(event) {
+  event.preventDefault();
+  form.querySelector(":user-invalid").focus();
+}
+
+const form = document.querySelector("form");
+const delOutput = document.querySelector("#del_output");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // 2. samle data op
+  const formData = new FormData(form);
+  const firstName = formData.get("del");
+
+  // 3. vise data
+  delOutput.textContent = firstName;
+
+  // 4. evt. nustil formular
+
+  form.reset();
+}
+form.addEventListener("invalid", cancelPopup, true);
+form.addEventListener("submit", handleSubmit);
